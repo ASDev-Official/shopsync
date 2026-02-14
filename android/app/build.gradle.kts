2a01d9697161cced00ce8e198a213cf9034c231b
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2026-02-14 23:06:34 +0800

    bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,18 +55,18 @@ android {
     productFlavors {
         create("phone") {
             val phoneBaseVersionCode = 300000000
-            val phoneVersionCode = 17
+            val phoneVersionCode = 18
             dimension = "platform"
             manifestPlaceholders.clear()
-            versionName = "5.2.1-phone"
+            versionName = "5.2.2-phone"
             versionCode = phoneBaseVersionCode + phoneVersionCode
         }
         create("wear") {
             val wearBaseVersionCode = 400000000
-            val wearVersionCode = 12
+            val wearVersionCode = 13
             manifestPlaceholders.clear()
             dimension = "platform"
-            versionName = "1.3.3-wear"
+            versionName = "1.3.4-wear"
             versionCode = wearBaseVersionCode + wearVersionCode
         }
     }
